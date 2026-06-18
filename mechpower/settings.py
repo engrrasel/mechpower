24dfd9a30c7c4dcd26082893508fb470a0201ada
Rasel Mia <eng.raselmiah@gmail.com>
2026-06-18 18:31:31 +0600

    Add dotenv configuration and update settings

--- mechpower/settings.py
+++ mechpower/settings.py
@@ -1,42 +1,42 @@
-"""
-Django settings for mechpower project.
-"""
-
 from pathlib import Path
 import os
 
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# =========================
+# ==================================
 # SECURITY
-# =========================
+# ==================================
 
-SECRET_KEY = os.environ.get(
+SECRET_KEY = os.getenv(
     "SECRET_KEY",
     "django-insecure-dev-key"
 )
 
-DEBUG = os.environ.get(
+DEBUG = os.getenv(
     "DEBUG",
-    "True"
+    "False"
 ) == "True"
 
 
 ALLOWED_HOSTS = [
     "mechpower.net",
     "www.mechpower.net",
-    "127.0.0.1",
+    "site-poszoi.hostnin.com",
     "localhost",
-    "172.31.40.45",
+    "127.0.0.1",
 ]
-
 
 CSRF_TRUSTED_ORIGINS = [
     "https://mechpower.net",
     "https://www.mechpower.net",
+    "https://site-poszoi.hostnin.com",
 ]
-
 
 SECURE_PROXY_SSL_HEADER = (
     "HTTP_X_FORWARDED_PROTO",
@@ -45,166 +45,147 @@ SECURE_PROXY_SSL_HEADER = (
 
 USE_X_FORWARDED_HOST = True
 
-
-# iframe / embed support
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
-
-# =========================
+# ==================================
 # APPS
-# =========================
+# ==================================
 
 INSTALLED_APPS = [
 
-    'jazzmin',
+    "jazzmin",
 
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 
-    'web',
-    'certificate',
+    "web",
+    "certificate",
 ]
 
-
-# =========================
+# ==================================
 # MIDDLEWARE
-# =========================
+# ==================================
 
 MIDDLEWARE = [
 
-    'django.middleware.security.SecurityMiddleware',
+    "django.middleware.security.SecurityMiddleware",
 
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
 
-    'django.middleware.common.CommonMiddleware',
+    "django.middleware.common.CommonMiddleware",
 
-    'django.middleware.csrf.CsrfViewMiddleware',
+    "django.middleware.csrf.CsrfViewMiddleware",
 
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
 
-    'django.contrib.messages.middleware.MessageMiddleware',
+    "django.contrib.messages.middleware.MessageMiddleware",
 
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+ROOT_URLCONF = "mechpower.urls"
 
-ROOT_URLCONF = 'mechpower.urls'
-
-
-# =========================
+# ==================================
 # TEMPLATES
-# =========================
+# ==================================
 
 TEMPLATES = [
-
     {
-        'BACKEND':
-        'django.template.backends.django.DjangoTemplates',
+        "BACKEND":
+        "django.template.backends.django.DjangoTemplates",
 
-        'DIRS': [
-            BASE_DIR / 'templates'
+        "DIRS": [
+            BASE_DIR / "templates"
         ],
 
-        'APP_DIRS': True,
+        "APP_DIRS": True,
 
-        'OPTIONS': {
+        "OPTIONS": {
+            "context_processors": [
 
-            'context_processors': [
+                "django.template.context_processors.request",
 
-                'django.template.context_processors.request',
+                "django.contrib.auth.context_processors.auth",
 
-                'django.contrib.auth.context_processors.auth',
-
-                'django.contrib.messages.context_processors.messages',
-
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
+WSGI_APPLICATION = "mechpower.wsgi.application"
 
-WSGI_APPLICATION = 'mechpower.wsgi.application'
-
-
-# =========================
+# ==================================
 # DATABASE
-# =========================
+# ==================================
 
 DATABASES = {
-    'default': {
-        'ENGINE':
-        'django.db.backends.sqlite3',
-
-        'NAME':
-        BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
-
-# =========================
-# PASSWORDS
-# =========================
+# ==================================
+# PASSWORD VALIDATION
+# ==================================
 
 AUTH_PASSWORD_VALIDATORS = [
-
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME":
+        "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-
     {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME":
+        "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
-
     {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME":
+        "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
-
     {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME":
+        "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
+# ==================================
+# INTERNATIONALIZATION
+# ==================================
 
-# =========================
-# LANGUAGE
-# =========================
+LANGUAGE_CODE = "en-us"
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = "Asia/Dhaka"
 
 USE_I18N = True
 
 USE_TZ = True
 
+# ==================================
+# STATIC FILES
+# ==================================
 
-# =========================
-# STATIC / MEDIA
-# =========================
-
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# ==================================
+# MEDIA FILES
+# ==================================
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
-
-# =========================
+# ==================================
 # DEFAULT
-# =========================
+# ==================================
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
